b53711a567891c4e1712264f7842355f29c75d37
lmalexander <elizabeth.alexander@northwestern.edu>
2019-12-11 17:29:09 -0600

    edited readme, added error function in scripts.js, added giraffe meme to assets

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// -----------------------------User Birthday and Zodiac Sign -----------------------------------
 let day = $("#day")
 let month = $("#month")
 let dayValue = day.val()
@@ -57,9 +58,6 @@ const signs =[{
     endDate: "03/20"
 }];
 
-
-
-
 $(day).on("change", function(){
     dayValue=this.value;
     console.log(dayValue);
@@ -76,132 +74,108 @@ $(month).on("change", function(){
     }
 })
 
-
 let signFinder = function (day, month) {
     let monthDay = `${month}/${day}`
     let starSign;
 
 // -------------------------NASA Pic/Video of the day - NH -------------------------------------------
 
-// convert birthdate for Query URL.
+// -------------------convert birthdate for Query URL.------------
 var urlDate = moment(monthDay, "MM/DD").format("MM-DD");
 var queryURL = "https://api.nasa.gov/planetary/apod?api_key=dS2HzlrfOwnYcGBTKbyf8Vz1QzgxwCbH2dLQ1uJ5&date=2018-" + urlDate
 
+//---------------- API pull.--------------------
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
 
-var media = response.media_type
+//-------------- Checking to see what the media type is-------------
+    var media = response.media_type
 
+//--------------------Show cards and Hide dropdowns-------------------
 $(".card").removeClass("hidden");
-$(".input-field").addClass("hidden");
+$(".select").addClass("hidden");
 
-// in case date pulls video, used alert just for testing .
+// ---------------in case date pulls video.-----------------------------------
 if (media != "image") {
-  alert("Your POTD is actually a video.  Please click the link to watch.");
+// ------------------Populating card,----------------------
+  $(".vid").text("Your 'Picture of the Day' is actually a video.   Please click the link below to watch.");
   $("#bDayImg").attr("src","");
   $("#imgLink").text("Click here to watch video"); 
   $("#imgLink").attr("href", response.url); 
 } else {  
   $("#bDayImg").attr("src", response.url); 
+// -------------function opens link in new tab-------------------------------
+  $("a.pop").click(function() {
+    window.open(this.href);
+    return false;
+  });
   $("#imgLink").attr("href", response.url); 
 }
   $(".card-title2").text(response.title); 
   $(".card-content2").text(response.explanation);
-  $(".card-content2").text(response.explanation);
+  $(".card-content2").text(response.explanation);  
 });
-// --------------------------------------------------------------
 
 //make a .then call to get the description from the returned ajax call and populate a corresponding html div class///id
     
+// ------------------------ Wikipedia Born on This Day - LA --------------------------------------
 
-    // append variables to Wikipedia queryURL search 
+// append variables to Wikipedia queryURL search 
     var queryURLWikipedia = "https://en.wikipedia.org/api/rest_v1/feed/onthisday/births/" + monthDay;
 
-    // check queryURL
+// check queryURL
     console.log(queryURLWikipedia);
 
-    // return response object data to card
+// return response object data to card
     $.ajax({
         url: queryURLWikipedia,
         method: "GET"
     }).then(function(Wikipediaresponse) {
         console.log(Wikipediaresponse); 
     // card text
-        let cardText = Wikipediaresponse.births[2].text;
+        let cardText = Wikipediaresponse.births[25].text;
         console.log(cardText);
         $("#text").text(cardText);
-        
     // card year
-        let cardYear = Wikipediaresponse.births[2].year;
+        let cardYear = Wikipediaresponse.births[25].year;
         console.log(cardYear);
-        $("#year").text(cardYear);
+        $("#year1").text(cardYear);
     // card thumbnail
-        let cardThumbnail = Wikipediaresponse.births[2].pages[0].originalimage.source;
-        console.log(cardThumbnail);
-        $("#thumbnail1").attr("src",cardThumbnail);
+        if (Wikipediaresponse.births[25].pages[0].originalimage) {
+            let cardThumbnail = Wikipediaresponse.births[25].pages[0].originalimage.source;
+            console.log(cardThumbnail);
+            $("#thumbnail1").attr("src" , cardThumbnail);  
+        } else {
+            console.log("No Wikipedia image!");
+            $("#thumbnail1").attr("src", "assets/giraffe-error-meme.jpeg");
+        } 
     }); // end ajax call 
-}
 
+// ---------------------------------- Horoscope ----------------------------------------------
 for (let i=0; i<signs.length; i++) {
         if (signs[i].startDate<= monthDay && monthDay <= signs[i].endDate) {
             starSign=signs[i].sign
             console.log(`Sign ${signs[i].sign}`)
             $("#star-sign").text(starSign)
             break;
-        }
-    }
-
-    console.log(starSign);
-    if(starSign){
-    
-
-        $.ajax({
+        }} console.log(starSign);
+        
+        if(starSign){
+             $.ajax({
             type:'POST',
             url:`https://aztro.sameerkumar.website?sign=${starSign}&day=today`,
             success:function(data){
             console.log(data);
             }
-       }).then(function (data) {
+            
+            }).then(function (data) {
            $(".date-range").text(`${data.date_range}`);
            $(".description").text(`${data.description}`);
            $(".compatibility").text(`Compatibility: ${data.compatibility}`);
            $(".mood").text(`Mood: ${data.mood}`);
            $(".color").text(`Color: ${data.color}`);
        })
-       
     }   
-       //make a .then call to get the description from the returned ajax call and populate a corresponding html div class///id
-    
-
-    // append variables to Wikipedia queryURL search 
-var queryURLWikipedia = "https://en.wikipedia.org/api/rest_v1/feed/onthisday/births/" + monthDay;
-
-    // check queryURL
-    console.log(queryURLWikipedia);
-
-    // return response object data to card
-    $.ajax({
-        url: queryURLWikipedia,
-        method: "GET"
-    }).then(function(Wikipediaresponse) {
-        console.log(Wikipediaresponse); 
-    // card text
-        let cardText = Wikipediaresponse.births[2].text;
-        console.log(cardText);
-        $("#text").text(cardText);
-        
-    // card year
-        let cardYear = Wikipediaresponse.births[2].year;
-        console.log(cardYear);
-        $("#year").text(cardYear);
-    // card thumbnail
-        let cardThumbnail = Wikipediaresponse.births[2].pages[0].originalimage.source;
-        console.log(cardThumbnail);
-        $("#thumbnail").attr("src",cardThumbnail);
-    }); // end ajax call 
-
-
-
-
+}
